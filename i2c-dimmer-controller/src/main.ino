d89e5abeade262954283966df60da005ca3bb1f9
Niall Gormley <demonday@armadillo.ie>
2018-01-24 21:16:23 +0000

    Refactored to store dimmer state in just one variable, rather than two.

--- i2c-dimmer-controller/src/main.ino
+++ i2c-dimmer-controller/src/main.ino
@@ -17,7 +17,7 @@
 
 #define I2C_SLAVE_ADDRESS 0x4
 
-#define DETECT PB4  //zero cross detect pin
+#define DETECT PB4  //zerocrossing detect pin
 #define GATE PB3    //TRIAC gate pin
 
 #define PULSE 2  //TRIAC trigger pulse width (in timer counts)
@@ -25,66 +25,69 @@
 #define MIN   40  //Min phase cut delay before keeping the traic switched ON
 #define MAX  150  //Max phase cut delay before keeping the triac switched OFF
 
-// Dim level is the amount of time in ticks that the TRIAC
-// remains OFF after zercross is detected
-int dim_level = MAX; // default dim level to max (OFF)
-
 // Brightness is on the scale from 0 thru 100
 // 0 = COMPLETLY OFF, 100 = COMPLETLY ON
-uint8_t brightness;
+uint8_t brightness = 0;
 
 //zero cross detect interrupt hander
 void zeroCrossingInterrupt() {
 
+  int delay = bri2dim(brightness);
+
   //Should we just leave the triac competely ON or OFF
-  if (dim_level <= MIN)
+  if (delay <= MIN)
   {
     digitalWrite(GATE, HIGH); //Leave On
   }
-  else if (dim_level >= MAX)
+  else if (delay >= MAX)
   {
     digitalWrite(GATE, LOW); //Leave Off
   }
   else
   {
-    OCR1A = dim_level;
+    OCR1A = delay;
     TCCR1 |= (1 << CTC1);
     TCCR1 |= (1 << CS13) | (1 << CS11); // Timer1/512
   }
 }
 
-// Time OFF delay after Zerocrossing is up, switch TRIAC ON, and set up
+// Time OFF delay after Zerocrossing is now up, switch TRIAC ON, and set
 // the timer start value to overflow once the pulse to triac has been sent
 ISR(TIMER1_COMPA_vect) {
-  TCCR1 = 0x00;             //Stop Timer
+  TCCR1 = 0x00;             //Stop Timer & Reset it
   TCCR1 |= (1 << CTC1);
-  TCCR1 |= (1 << CS13) | (1 << CS11);  // Timer1/512  (15625 Hz based on 8MHz Clock)
-  digitalWrite(GATE, HIGH); //set TRIAC gate to high
-  TCNT1 = 256 - PULSE;      //trigger overflow for TRIAC pulse width
+  TCCR1 |= (1 << CS13) | (1 << CS11);
+  digitalWrite(GATE, HIGH); //set TRIAC gate to high (ON)
+  TCNT1 = 256 - PULSE;      //set timer to trigger overflow after TRIAC pulse width time
 }
 
-// Pluse has bee sent, now turn off the TRIAC
+// Pluse has bee sent, now switch off the gate of the
+// TRIAC, which will switch off emmitter at next zerocrossing.
 ISR(TIMER1_OVF_vect) {
   TCCR1 = 0x00;
   digitalWrite(GATE, LOW);
 }
 
-// Set brightness based on 0 thru 100
+// Convert brightness level to an amount of time in ticks that the TRIAC
+// remains OFF after zerocrossing is detected
 // 0  = off
-// 100  = max brightness
-void bri2dim()
+// 100  = max brightness (ON)
+int bri2dim(uint8_t brightness)
 {
-  float scale = (MAX - MIN) / 100;
-  dim_level = MAX - (uint8_t)(scale * brightness);
+  float scale = (MAX - MIN) / 101;
+  return MAX - (uint8_t)(scale * brightness);
 }
 
 // Receive an i2c instruction from Master. An instruction is just a uint8_t
-// The byte indicates the brightness on a scale from 0 theu 100,
-// with 100 being completly off, and 0 being completly on
-void setBrightness(uint8_t byte_count)
+// The byte indicates the brightness on a scale from 0 thru 100,
+// with 100 being completly ON, and 0 being completly OFF
+void setBrightnessLevel(uint8_t byte_count)
 {
-  //first byte is command, next 2 bytes is data for command. Total must equal 3 bytes.
   if (!Wire.available()) return;
+
+  // If we have more than one byte we dont know what to do
+  // so ingore for now. It should be corrected by the calling system
+  // if the current brightness dosent match the required brightness
   int i = 0;
   if (byte_count > 1)
   {
@@ -95,37 +98,39 @@ void setBrightness(uint8_t byte_count)
     }
   }
 
-  // input = 0 thru 100
+  // expect input values 0 thru 100
   brightness = Wire.read();
 
-  // Clamp to 100
+  // Clamp to 100 as max
   if(brightness>100)
   {
     brightness=100;
   }
 
-  // Turn the Brightness into a dim_level
-  bri2dim();
 }
 
 
 // Return the current brightness via i2c
-void getBrightness()
+void sendBrightnessResponse()
 {
   Wire.write(brightness);
 }
 
 void setup() {
 
-  // Setup i2C
-  Wire.begin(I2C_SLAVE_ADDRESS);  // join i2c network
-  Wire.onReceive(setBrightness);  // Recieve a command from the master (set input brightness)
-  Wire.onRequest(getBrightness);  // Service a request from the master (get input brightness)
+  // Setup i2c
+  Wire.begin(I2C_SLAVE_ADDRESS);  // join i2c bus
+
+  // Recieve a command from the master (set input brightness)
+  Wire.onReceive(setBrightnessLevel);
+
+  // Service a request from the master (to get current brightness)
+  Wire.onRequest(sendBrightnessResponse);
 
   pinMode(DETECT, INPUT_PULLUP);  //Zerocrossing Input
   pinMode(GATE, OUTPUT);          //TRIAC gate control
 
-  //Enable Timer1 comparator & overflow
+  //Enable Timer1 comparator & overflow functions
   TIMSK |= (1 << OCIE1A) | (1 << TOIE1);
 
   // Attach the interrupt handler to the pin that detects Zerocrossing
